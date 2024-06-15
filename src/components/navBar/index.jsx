15655837c2e0648c22ef15faee732190fee041bc
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-15 17:47:05 +0530

    navbar done

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import {FaBars, FaReact} from 'react-icons/fa';
 import {HiX} from 'react-icons/hi'
 import {Link} from 'react-router-dom'
-
+import './styles.scss';
 const data = [
     {
         label : 'HOME',
@@ -45,7 +45,8 @@ const Navbar =()=>{
                 <FaReact size={30}/>
                 </Link>
             </div>
-            <ul className="navbar__container__menu">
+            <ul className={`navbar__container__menu ${toggleIcon ? "active" : ""}`}>
+
                 {
                     data.map((item, key)=> (
                         <li key={key} className="navbar__container__menu__item">
